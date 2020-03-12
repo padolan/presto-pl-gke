7dbd5535e350155e70472f13512102e0d2bdb7e6
Patrick Dolan <Patrick.Dolan@walmart.com>
2020-03-11 22:14:11 -0500

    repo cleanup

--- hive-metastore/files/opt/sql/postgres/003-version.sql
+++
@@ -1,4 +0,0 @@
--- -----------------------------------------------------------------
--- Record schema version. Should be the last step in the init script
--- -----------------------------------------------------------------
-INSERT INTO "VERSION" ("VER_ID", "SCHEMA_VERSION", "VERSION_COMMENT") VALUES (1, '1.2.1000', 'Hive release version 1.2.1000');
--- salt-minion-centos/README.md
+++
@@ -1,5 +0,0 @@
-# Salt-minion (centos 7 based)
-
-Salt minion docker container based on Centos 7. Installed packages are:
-* `salt-minion`
-* `docker`
